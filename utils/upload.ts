99c3db80ef456638ac65c0b17191b608f26ca897
Mohammad Azhari <mazharioffical@gmail.com>
2025-01-14 00:16:33 +0400

    Add upload to S3 Sample comment

--- utils/upload.ts
+++ utils/upload.ts
@@ -43,3 +43,32 @@ const fileFilter: (allowedTypes?: string[]) => multer.Options["fileFilter"] =
       cb(new Error("Invalid file type. Only JPEG, PNG, and PDF are allowed."));
     }
   };
+
+/*
+  If we want to upload to cloud storage we can use multer-s3 as following:
+
+  import AWS from "aws-sdk";
+  import multer from "multer";
+  import multerS3 from "multer-s3";
+
+  // Configure AWS S3
+  const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+  });
+
+  // Create the S3 Multer storage
+  const s3Storage = multerS3({
+    s3,
+    bucket: process.env.AWS_BUCKET_NAME || "my-bucket",
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      cb(null, `uploads/${Date.now().toString()}_${file.originalname}`);
+    },
+  });
+
+  and use this s3Storage in getUploadMiddleware instead of diskStorage
+*/
